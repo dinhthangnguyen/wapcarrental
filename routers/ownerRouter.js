a24cc0468b81564d5301916514b9097b4d070182
Hieu-MIU <gtran@miu.edu>
2023-10-22 10:49:28 -0500

    remove script from html

--- routers/ownerRouter.js
+++ routers/ownerRouter.js
@@ -15,8 +15,10 @@ router.put('/api/:id', ownerController.updateOwner);
 router.use("/:id", async (req,res,next)=> {
     let frameSet = await fs.readFile(path.join(__dirname, ".." , "public", "view", "common", "index.html"));
     let component = await fs.readFile(path.join(__dirname, ".." , "public", "view", "owner", "manage.html"));
+    let script = await fs.readFile(path.join(__dirname, ".." , "public", "view", "owner", "manage.js"));
 
     let htmlContent = frameSet.toString().replace('<div class="row main-content">',`<div class="row main-content">${component.toString()}`);
+    htmlContent = htmlContent.replace('<script id="additional-script">',`<script id="additional-script">${script.toString()}`);
     res.send(htmlContent);
 });
 
@@ -24,8 +26,11 @@ router.use("/", async (req,res,next)=> {
     console.log(path.join(__dirname, "public", "view", "common", "index.html"));
     let frameSet = await fs.readFile(path.join(__dirname, ".." , "public", "view", "common", "index.html"));
     let component = await fs.readFile(path.join(__dirname, ".." , "public", "view", "owner", "index.html"));
+    let script = await fs.readFile(path.join(__dirname, ".." , "public", "view", "owner", "index.js"));
 
     let htmlContent = frameSet.toString().replace('<div class="row main-content">',`<div class="row main-content">${component.toString()}`);
+    htmlContent = htmlContent.replace('<script id="additional-script">',`<script id="additional-script">${script.toString()}`);
+    console.log(htmlContent);
     res.send(htmlContent);
 });
 
